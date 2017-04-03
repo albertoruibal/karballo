d7187d5ff2214007b0b670f74d791bc6b94f9708
albertoruibal <alberto@alonsoruibal.com>
2017-04-03 23:05:15 +0200

    Remove empty constructor

--- src/main/kotlin/karballo/book/NoBook.kt
+++ src/main/kotlin/karballo/book/NoBook.kt
@@ -3,7 +3,7 @@ package karballo.book
 import karballo.Board
 import karballo.Move
 
-class NoBook() : Book {
+class NoBook : Book {
 
     override fun getMove(board: Board): Int {
         return Move.NONE
